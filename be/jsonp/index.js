bb5b1d2c6a1f3756c4fc7d5dda04e9bc6b30310e
luoquanquan <luoquanquan@baidu.com>
2018-12-05 20:12:33 +0800

    改变后端代码, 返回正常的js语句

--- be/jsonp/index.js
+++ be/jsonp/index.js
@@ -1,12 +1,13 @@
-var http = require('http');
-var PORT = 8888
+const http = require('http');
+
+const PORT = 8888;
 
 // 创建一个 http 服务
-var server = http.createServer(function(request, response) {
-    response.end('hello world')
-})
+const server = http.createServer((request, response) => {
+  response.end("console.log('hello world')");
+});
 
 // 启动服务, 监听端口
-server.listen(PORT, function() {
-    console.log('服务启动成功, 正在监听: ', PORT)
-})
+server.listen(PORT, () => {
+  console.log('服务启动成功, 正在监听: ', PORT);
+});
