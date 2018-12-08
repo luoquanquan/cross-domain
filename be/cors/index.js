3ebead3f6231df75eef65b25c5c4543b19cf49fb
luoquanquan <luoquanquan@baidu.com>
2018-12-08 12:35:36 +0800

    请求携带 Cookie

--- be/cors/index.js
+++ be/cors/index.js
@@ -3,16 +3,22 @@ const http = require('http');
 const PORT = 8888;
 
 // 协议名必填, 如果同时存在 http 和 https 就写两条s
-const allowOrigin = ['http://127.0.0.1:8080', 'https://www.baidu.com'];
+const allowOrigin = ['http://127.0.0.1:8080', 'http://localhost:8080', 'https://www.baidu.com'];
 
 // 创建一个 http 服务
 const server = http.createServer((request, response) => {
-  const { headers: { origin } } = request;
+  const { method, headers: { origin, cookie } } = request;
   if (allowOrigin.includes(origin)) {
     response.setHeader('Access-Control-Allow-Origin', origin);
   }
   response.setHeader('Access-Control-Allow-Methods', 'PUT');
+  response.setHeader('Access-Control-Allow-Credentials', true);
   response.setHeader('Access-Control-Allow-Headers', 'token');
+  if (method === 'OPTIONS') {
+    console.log('预检请求');
+  } else if (!cookie) {
+    response.setHeader('Set-Cookie', 'quanquan=fe');
+  }
   response.end("{name: 'quanquan', friend: 'guiling'}");
 });
 
