8aae5963cb1afc48014ca355b844be62cc0a6e78
luoquanquan <luoquanquan@baidu.com>
2018-12-08 13:37:20 +0800

    预检请求不返回内容

--- be/cors/index.js
+++ be/cors/index.js
@@ -14,9 +14,11 @@ const server = http.createServer((request, response) => {
   response.setHeader('Access-Control-Allow-Methods', 'PUT');
   response.setHeader('Access-Control-Allow-Credentials', true);
   response.setHeader('Access-Control-Allow-Headers', 'token');
+  response.setHeader('Access-Control-Expose-Headers', 'token');
   response.setHeader('token', 'quanquan');
   if (method === 'OPTIONS') {
-    console.log('预检请求');
+    response.writeHead(204);
+    response.end('');
   } else if (!cookie) {
     response.setHeader('Set-Cookie', 'quanquan=fe');
   }
