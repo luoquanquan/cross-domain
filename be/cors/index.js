e2e1621135901ed7a48cb8064e2b0adac6f3c47d
luoquanquan <luoquanquan@baidu.com>
2018-12-08 11:43:38 +0800

    跨域访问的域名限制

--- be/cors/index.js
+++ be/cors/index.js
@@ -2,10 +2,15 @@ const http = require('http');
 
 const PORT = 8888;
 
+// 协议名必填, 如果同时存在 http 和 https 就写两条s
+const allowOrigin = ['http://127.0.0.1:8080', 'https://www.baidu.com'];
+
 // 创建一个 http 服务
 const server = http.createServer((request, response) => {
-//   response.writeHead
-  response.setHeader('Access-Control-Allow-Origin', '*');
+  const { headers: { origin } } = request;
+  if (allowOrigin.includes(origin)) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+  }
   response.setHeader('Access-Control-Allow-Methods', 'PUT');
   response.setHeader('Access-Control-Allow-Headers', 'token');
   response.setHeader('Access-Control-Max-Age', 5);
