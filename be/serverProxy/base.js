9b53deab8d22ca5558185347b7f0587db3f443c8
luoquanquan <luoquanquan@baidu.com>
2018-12-15 23:25:53 +0800

    前端访问同一项目接口后端跑通

--- be/serverProxy/base.js
+++ be/serverProxy/base.js
@@ -4,11 +4,26 @@ const koaStatic = require('koa-static');
 const app = new Koa();
 const PORT = 1234;
 
-app.use(koaStatic(`${__dirname}/www`));
+app.use(koaStatic('./www'));
 
 app.use((ctx) => {
-  console.log(ctx.req.url);
-  ctx.body = 'Hello World';
+  let ret;
+  // 获取本次接收的请求的请求路径
+  const path = ctx.req.url;
+
+  // 如果请求路径以api开头, 那么作为接口请求处理
+  if (path.startsWith('/api')) {
+    // 这样实现的路由不是很优雅, 但是能用 😂
+    switch (path) {
+      case '/api/getFriend':
+        ret = { name: 'quanquan', friend: 'gl' };
+        break;
+      default:
+        ret = { errno: 1, errmsg: '未知接口' };
+        break;
+    }
+  }
+  ctx.body = ret;
 });
 
 app.listen(PORT, () => {
